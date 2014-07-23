dc2333a2009cf6c63b963a4a0fc4223e63c6e4ef
Lee Tobin <leetobin@gmail.com>
2014-07-23 16:30:41 +0100

    System now uses iSCSI

--- extraFiles/scripts/evidenceExport.sh
+++ extraFiles/scripts/evidenceExport.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 #
 # Firebrick 
-# last edit 17/06/2013 - Lee Tobin
+# last edit 23/07/2014 - Lee Tobin
 
-#create the iblock backstore
-	echo "Creating /sys/kernel/config/target/core/iblock_0/sbpStore"
-mkdir -p /sys/kernel/config/target/core/iblock_0/sbpStore
-	echo "Adding udev_path=/dev/$evidenceDisk to control"
-echo "udev_path=/dev/$evidenceDisk" > /sys/kernel/config/target/core/iblock_0/sbpStore/control
+export CONFIGFS=/sys/kernel/config
+export TARGET=/sys/kernel/config/target/core
+export FABRIC=/sys/kernel/config/target/iscsi
 
-#echo "readonly=1" > /sys/kernel/config/target/core/iblock_0/sbpStore/control
-	echo "Enabling /sys/kernel/config/target/core/iblock_0/sbpStore/enable"
-echo 1 > /sys/kernel/config/target/core/iblock_0/sbpStore/enable
+#create a IBLOCK HBA and virtual storage object
+mkdir -p $TARGET/iblock_0/lvm_test0
+# Tell the virtual storage object what struct block_device we want
+echo "udev_path=/dev/$evidenceDisk" > $TARGET/iblock_0/lvm_test0/control
+echo "readonly=1" > $TARGET/iblock_0/lvm_test0/control
+# Enable the virtual storage object and call bd_claim()
+echo 1 > $TARGET/iblock_0/lvm_test0/enable
 
-#create the sbp2 target
-	echo "Creating /sys/kernel/config/target/sbp/0011066600000009/tpgt_1/lun/lun_0"
-mkdir -p /sys/kernel/config/target/sbp/0011066600000009/tpgt_1/lun/lun_0
-	echo "Creating link"
-ln -s /sys/kernel/config/target/core/iblock_0/sbpStore /sys/kernel/config/target/sbp/0011066600000009/tpgt_1/lun/lun_0/sbpStore
-	echo "Enabling /sys/kernel/config/target/sbp/0011066600000009/tpgt_1/enable"
-echo 1 > /sys/kernel/config/target/sbp/0011066600000009/tpgt_1/enable
+DEF_IQN="iqn.2003-01.org.linux-iscsi.target.i686:sn.e475ed6fcdd0"
+
+#create the network portal on $DEF_IQN/tpgt_1
+mkdir -p "$FABRIC/$DEF_IQN/tpgt_1/np/192.168.0.1:3260"
+# Create LUN 0 on $DEF_IQN/tpgt_1
+mkdir -p "$FABRIC/$DEF_IQN/tpgt_1/lun/lun_0"
+# Create the iSCSI Target Port Mapping for $DEF_IN/tpgt_1 LUN 0
+# to lvm_test0 and give it the port symbolic name of lio_west_port
+ln -s $TARGET/iblock_0/lvm_test0 "$FABRIC/$DEF_IQN/tpgt_1/lun/lun_0/lio_west_port"
+
+# Allow iSCSI Initiators to login to $DEF_IQN/tpgt_1
+#warning Currently uses generate_node_acls=1,cache_dynamic_acls=1,demo_mode_lun_access=1
+echo 1 > $FABRIC/$DEF_IQN/tpgt_1/enable
+
