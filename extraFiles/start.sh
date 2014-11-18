aa22c3b18c4cd02fe3c59d03e0c0f210ab4613f0
leetobin <leetobin@gmail.com>
2014-11-18 16:32:42 +0000

    Adding the quickrebuild make target label.
    This allows you to do a quick rebuild, once you've built the FIREBrick you can then rebuild the target filesystem by running "make quickrebuild"

--- extraFiles/start.sh
+++ extraFiles/start.sh
@@ -10,6 +10,15 @@ else
 	sed -i '/^menu "Target packages"/a source "package/dcfldd/Config.in"' buildroot/package/Config.in
 fi
 
+#Add quick rebuild to buildroot Makefile
+if grep -F 'quickrebuild' buildroot/Makefile ; then
+	echo 'quickrebuild already in Buildroot Makefile'
+else
+	echo 'Added quickrebuild to Buildroot Makefile'
+	line='rm -rf $(TARGET_DIR) $(STAGING_DIR) $(STAMP_DIR) $(BUILD_DIR)/.root $(BUILD_DIR)/*/.stamp_target_installed $(BUILD_DIR)/*/.stamp_staging_installed $(BUILD_DIR)/linux-*/.stamp_installed $(BUILD_DIR)/*/.built'
+	echo -e "\nquickrebuild:\n\t$line\n" >> buildroot/Makefile
+fi
+
 cd buildroot
 make
 #make V=s 2>&1 | tee build.log | grep -i error
